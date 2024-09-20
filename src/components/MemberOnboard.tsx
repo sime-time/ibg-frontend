ef0c742c8271a94b64d73ade3780301aca813647
Dunn, Simeon <simedunn01@gmail.com>
2024-09-20 18:15:35 -0400

    started member onboarding comp

---
+++ src/components/MemberOnboard.tsx
@@ -0,0 +1,6 @@
+export default function MemberOnboard() {
+  return (
+    <div>Member onboarding</div>
+  );
+}
+
--- src/routes/dashboard-member.tsx
+++ src/routes/dashboard-member.tsx
@@ -1,12 +1,17 @@
 import { clientOnly } from "@solidjs/start";
 
 const MemberDashboard = clientOnly(() => import("~/components/MemberDashboard"));
+const MemberOnboard = clientOnly(() => import("~/components/MemberOnboard"));
 
 export default function DashboardMember() {
+  // determine if member needs to be onboarded (no subscription)
+  const isSubscribed: boolean = false;
+
+
   return (
     <main class="m-auto p-4 flex flex-col gap-6 items-center">
       <h1 class="text-6xl text-red-600/90 text-center font-thin uppercase">Member Dashboard</h1>
-      <MemberDashboard />
+      {isSubscribed ? <MemberDashboard /> : <MemberOnboard />}
     </main>
   );
 }
