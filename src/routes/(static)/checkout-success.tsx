ccedbfba09dff2f57a1c7da571e812c4ae85af15
Dunn, Simeon <simedunn01@gmail.com>
2024-09-26 11:47:08 -0400

    checkout success/cancel pages

---
+++ src/routes/(static)/checkout-success.tsx
@@ -0,0 +1,7 @@
+export default function CheckoutSuccess() {
+  return (
+    <main class="text-center m-auto p-4">
+      <h1 class="text-6xl text-red-600/90 font-thin uppercase ">Checkout Successful</h1>
+    </main>
+  );
+}
