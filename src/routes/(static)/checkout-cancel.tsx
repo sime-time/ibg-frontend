f85bfbd30af42cc35d0c68e38cbeaaa215dce317
Dunn, Simeon <simedunn01@gmail.com>
2024-10-20 19:22:16 -0400

    use default 'a' href in checkout-success

--- src/routes/(static)/checkout-cancel.tsx
+++ src/routes/(static)/checkout-cancel.tsx
@@ -1,11 +1,9 @@
-import { A } from "@solidjs/router";
-
 export default function CheckoutCancel() {
   return (
     <main class="m-auto p-4 flex flex-col gap-6 items-center">
       <h1 class="text-6xl text-red-600/90 font-thin uppercase ">Checkout Cancelled</h1>
       <div>
-        <A href="/" class="rounded-md bg-red-600/90 hover:bg-red-700 text-white py-3 px-5">Return to Home page</A>
+        <a href="/" class="rounded-md bg-red-600/90 hover:bg-red-700 text-white py-3 px-5">Return to Home page</a>
       </div>
     </main>
   );
--- src/routes/(static)/checkout-success.tsx
+++ src/routes/(static)/checkout-success.tsx
@@ -1,11 +1,9 @@
-import { A } from "@solidjs/router";
-
 export default function CheckoutSuccess() {
   return (
     <main class="m-auto p-4 flex flex-col gap-6 items-center">
       <h1 class="text-6xl text-red-600/90 font-thin uppercase ">Checkout Successful</h1>
       <div>
-        <A href="/member" class="rounded-md bg-red-600/90 hover:bg-red-700 text-white py-3 px-5">Go to your dashboard</A>
+        <a href="/member" class="rounded-md bg-red-600/90 hover:bg-red-700 text-white py-3 px-5">Go to your dashboard</a>
       </div>
     </main>
   );
