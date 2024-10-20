8ac882e9082b2a72a91cd1b0d13ed31b6e3720d5
Dunn, Simeon <simedunn01@gmail.com>
2024-10-20 18:06:52 -0400

    switch/match on member page

--- src/routes/member.tsx
+++ src/routes/member.tsx
@@ -1,5 +1,6 @@
 import { clientOnly } from "@solidjs/start";
 import Pocketbase from "pocketbase";
+import { createSignal, Match, Switch } from "solid-js";
 
 const AccessDenied = clientOnly(() => import("~/components/AccessDenied"))
 const OnboardForm = clientOnly(() => import("~/components/forms/OnboardForm"))
@@ -10,20 +11,32 @@ const pb = new Pocketbase(import.meta.env.VITE_POCKETBASE_URL);
 
 export default function MemberPage() {
   const member = pb.authStore.model;
+  const [hasBirthDate, setHasBirthDate] = createSignal<boolean>(false);
+  const [isSubscribed, setIsSubscribed] = createSignal<boolean>(false);
+
   if (!pb.authStore.isValid || pb.authStore.isAdmin) {
     return <main><AccessDenied /></main>;
-
-  } else if (!member?.birth_date) {
-    return <main><OnboardForm memberName={member?.name} /></main>;
-
-  } else if (!member?.is_subscribed) {
-    return <main><ChooseProgram customerId={String(member?.stripe_customer_id)} /></main>
-
   } else {
-    return (
-      <main class="m-auto p-4 flex flex-col gap-6 items-center w-full">
-        <MemberDashboard pb={pb} />
-      </main>
-    );
+    console.log("Birthdate: ", member?.birth_date);
+    console.log("Is Subscribed: ", member?.is_subscribed);
+
+    setHasBirthDate(Boolean(member?.birth_date));
+    setIsSubscribed(Boolean(member?.is_subscribed));
   }
+
+  return (
+    <main class="m-auto p-4 flex flex-col gap-6 items-center w-full">
+      <Switch>
+        <Match when={hasBirthDate() == false}>
+          <OnboardForm memberName={member?.name} />
+        </Match>
+        <Match when={isSubscribed() == false}>
+          <ChooseProgram customerId={String(member?.stripe_customer_id)} />
+        </Match>
+        <Match when={hasBirthDate() && isSubscribed()}>
+          <MemberDashboard pb={pb} />
+        </Match>
+      </Switch>
+    </main>
+  );
 }
