c70f6b92a5c777fee054215dfdaed97136660c4d
Dunn, Simeon <simedunn01@gmail.com>
2024-10-25 20:41:39 -0400

    fix checkout-success page

--- src/routes/checkout-success.tsx
+++ src/routes/checkout-success.tsx
@@ -1,8 +1,8 @@
 export default function CheckoutSuccess() {
   return (
     <main class="flex flex-col gap-4 mt-24 text-center">
-      <h2 class="text-2xl font-bold">Checkout Successful - Thank You!</h2>
-      <a href="/member" class="btn btn-primary">Continue to your dashboard</a>
+      <h2 class="text-2xl font-bold text-green-300">Checkout Successful - Thank You!</h2>
+      <div><a href="/member" class="btn btn-primary">Go to your dashboard</a></div>
     </main>
   );
 }
