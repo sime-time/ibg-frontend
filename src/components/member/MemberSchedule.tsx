8170f7863396d55c8bfc263c8a6fc0f1b4b197ca
Dunn, Simeon <simedunn01@gmail.com>
2024-10-28 16:32:03 -0400

    page not found restyled

--- src/components/member/MemberSchedule.tsx
+++
@@ -1,5 +0,0 @@
-export default function MemberSchedule() {
-  return (
-    <></>
-  );
-}
--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,7 +4,10 @@ export default function NotFound() {
   return <>
     <Title>Page Not Found</Title>
     <main>
-      <h1>Page Not Found</h1>
+      <div class="flex flex-col gap-4 mt-24 text-center">
+        <h2 class="text-2xl font-bold">Page Not Found</h2>
+        <a href="/" class="text-lg underline text-error">Return to Home Page</a>
+      </div>
     </main>
   </>
 }
