4fb0502f84af0b5d7015ce1f90781e431e2cadde
Dunn, Simeon <simedunn01@gmail.com>
2024-12-20 22:25:38 -0500

    put service worker registration in entry-client.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -7,20 +7,6 @@ import Nav from "~/components/Nav";
 import "./app.css";
 
 export default function App() {
-  onMount(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-          .then(registration => {
-            console.log('Service Worker registered:', registration);
-          })
-          .catch(error => {
-            console.error('Service Worker registration failed:', error);
-          });
-      });
-    }
-  });
-
   return (
     <MetaProvider>
       <PocketbaseContextProvider>
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,17 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
+// service worker registration
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
 mount(() => <StartClient />, document.getElementById("app")!);
