28be293702007276ad6fb4ea7b729eaff69aa066
Dunn, Simeon <simedunn01@gmail.com>
2025-01-05 22:05:21 -0500

    starting pricing table for signup

---
+++ src/components/member/MemberPlans.tsx
@@ -0,0 +1,3 @@
+export default function MemberPlans() {
+  return (<>Member plans</>);
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { Title } from "@solidjs/meta";
+import MemberPlans from "~/components/member/MemberPlans";
 
 export default function Home() {
   return <>
     <Title>Indy Boxing and Grappling</Title>
     <main class="flex justify-center items-center gap-5">
-      <a href="/signup" class="btn btn-accent">Sign Up</a>
-      <a href="/login" class="btn btn-accent btn-outline">Log In</a>
+      <MemberPlans />
     </main>
   </>
 }
