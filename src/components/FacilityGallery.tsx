e2bc8f87657740467f49e87ec275dabf5a64e5cf
Dunn, Simeon <simedunn01@gmail.com>
2025-01-22 15:35:26 -0500

    optimize facility images

--- src/components/FacilityGallery.tsx
+++ src/components/FacilityGallery.tsx
@@ -5,18 +5,21 @@ export default function FacilityGallery() {
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6 items-center justify-center">
         <img
           class="md:col-span-2"
-          src="/images/facility/heavybagroom.jpg"
+          src="/images/facility/heavybagroom.webp"
           alt="heavy bag room"
         />
-        <img src="/images/facility/sparring.jpg" alt="sparring session" />
-        <img src="/images/facility/ibg-ring.jpg" alt="boxing ring" />
+        <img src="/images/facility/sparring.webp" alt="sparring session" />
+        <img src="/images/facility/ibg-ring.webp" alt="boxing ring" />
         <img
           class="md:col-span-2"
-          src="/images/facility/mattroom.jpg"
+          src="/images/facility/mattroom.webp"
           alt="matt room"
         />
-        <img src="/images/facility/jiu-jitsu-class.jpg" alt="jiu jitsu class" />
-        <img src="/images/facility/mma-class.jpg" alt="martial arts class" />
+        <img
+          src="/images/facility/jiu-jitsu-class.webp"
+          alt="jiu jitsu class"
+        />
+        <img src="/images/facility/mma-class.webp" alt="martial arts class" />
       </div>
     </div>
   );
