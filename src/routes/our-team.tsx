9ed10fb7dff32b42fb38c36b881c523991adc158
Dunn, Simeon <simedunn01@gmail.com>
2025-07-16 16:32:45 -0400

    add back our team for seo

---
+++ src/routes/our-team.tsx
@@ -0,0 +1,13 @@
+import { Title } from "@solidjs/meta"
+
+export default function OurTeam() {
+  return (<>
+    <Title>Our Team - Indy Boxing and Grappling</Title>
+    <main>
+      <div class="flex flex-col gap-4 mt-24 text-center mx-4">
+        <h2 class="text-2xl font-bold">🚧 This page is under construction 🚧</h2>
+        <a href="/" class="text-lg underline text-error">Return to Home Page</a>
+      </div>
+    </main>
+  </>);
+}
